dd3bbd68f2b4c66e8a0860bdca809bfb3649594f
LeftTwixWand <0965142838gvs@gmail.com>
2025-04-01 01:30:41 +0200

    New MCP setup

--- AzureDevOps.MCP.Web/Program.cs
+++ AzureDevOps.MCP.Web/Program.cs
@@ -1,43 +1,53 @@
-using AzureDevOps.MCP.Web;
+using AzureDevOps.MCP.ServiceDefaults;
 using AzureDevOps.MCP.Web.Components;
+using ModelContextProtocol;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Transport;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddSingleton<IMcpClient>(serviceProvider =>
+{
+	McpClientOptions mcpClientOptions = new()
+	{ ClientInfo = new() { Name = "AspNetCoreSseClient", Version = "1.0.0" } };
 
-builder.Services.AddOutputCache();
+	HttpClient httpClient = new()
+	{
+		BaseAddress = new("https://localhost:7515/sse")  //"https +http://aspnetsseserver" + "/sse")
+	};
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
+	McpServerConfig mcpServerConfig = new()
+	{
+		Id = "AspNetCoreSse",
+		Name = "AspNetCoreSse",
+		TransportType = TransportTypes.Sse,
+		Location = httpClient.BaseAddress.ToString(),
+	};
+
+	var mcpClient = McpClientFactory.CreateAsync(mcpServerConfig, mcpClientOptions).GetAwaiter().GetResult();
+	return mcpClient;
+});
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
-app.UseOutputCache();
-
 app.MapStaticAssets();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+	.AddInteractiveServerRenderMode();
 
 app.MapDefaultEndpoints();
 
